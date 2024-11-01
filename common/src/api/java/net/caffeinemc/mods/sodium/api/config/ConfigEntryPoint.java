2ed12591c500b1573e3ddb628851b36b63315b89
douira <douira100@gmail.com>
2024-11-01 17:37:49 +0100

    add default implementation to early register entrypoint so users don't need to implement both entrypoints even if they only need the late ("main") one

--- common/src/api/java/net/caffeinemc/mods/sodium/api/config/ConfigEntryPoint.java
+++ common/src/api/java/net/caffeinemc/mods/sodium/api/config/ConfigEntryPoint.java
@@ -3,7 +3,8 @@ package net.caffeinemc.mods.sodium.api.config;
 import net.caffeinemc.mods.sodium.api.config.structure.ConfigBuilder;
 
 public interface ConfigEntryPoint {
-    void registerConfigEarly(ConfigBuilder builder);
+    default void registerConfigEarly(ConfigBuilder builder) {
+    };
 
     void registerConfigLate(ConfigBuilder builder);
 }
