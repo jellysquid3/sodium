46efd91759bc97b7e0ae944ab94d08607ad49f3c
douira <douira@users.noreply.github.com>
2024-11-24 23:05:43 +0100

    Sort render lists for regions and sections after traversal (#2780)
    Render sections and regions are sorted after the graph traversal is performed. This decouples their ordering from the graph, which isn't entirely correct for draw call sorting.
    
    Fixes #2266

--- common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/occlusion/OcclusionCuller.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/occlusion/OcclusionCuller.java
@@ -50,12 +50,11 @@ public class OcclusionCuller {
         RenderSection section;
 
         while ((section = readQueue.dequeue()) != null) {
-            boolean visible = isSectionVisible(section, viewport, searchDistance);
-            visitor.visit(section, visible);
-
-            if (!visible) {
+            if (!isSectionVisible(section, viewport, searchDistance)) {
                 continue;
             }
+
+            visitor.visit(section);
 
             int connections;
 
@@ -249,7 +248,7 @@ public class OcclusionCuller {
         section.setLastVisibleFrame(frame);
         section.setIncomingDirections(GraphDirectionSet.NONE);
 
-        visitor.visit(section, true);
+        visitor.visit(section);
 
         int outgoing;
 
@@ -335,6 +334,6 @@ public class OcclusionCuller {
     }
 
     public interface Visitor {
-        void visit(RenderSection section, boolean visible);
+        void visit(RenderSection section);
     }
 }
