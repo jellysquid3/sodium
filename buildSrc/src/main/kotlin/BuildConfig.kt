e6ccbe8d500f3b4bb8902d8dffe7f45252ff9300
JellySquid <jellysquid@pm.me>
2024-12-02 14:25:41 -0600

    Bump dependency versions

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -2,9 +2,9 @@ import org.gradle.api.Project
 
 object BuildConfig {
     val MINECRAFT_VERSION: String = "1.21.1"
-    val NEOFORGE_VERSION: String = "21.1.46"
-    val FABRIC_LOADER_VERSION: String = "0.16.4"
-    val FABRIC_API_VERSION: String = "0.103.0+1.21.1"
+    val NEOFORGE_VERSION: String = "21.1.83"
+    val FABRIC_LOADER_VERSION: String = "0.16.9"
+    val FABRIC_API_VERSION: String = "0.110.0+1.21.1"
 
     // This value can be set to null to disable Parchment.
     // TODO: Re-add Parchment
