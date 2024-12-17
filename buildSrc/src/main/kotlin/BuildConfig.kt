e0c7108d20909648e762a6a5f600bbfc9d1e6b41
JellySquid <jellysquid@pm.me>
2024-12-17 14:09:38 -0600

    Bump version and dependencies

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -2,7 +2,7 @@ import org.gradle.api.Project
 
 object BuildConfig {
     val MINECRAFT_VERSION: String = "1.21.1"
-    val NEOFORGE_VERSION: String = "21.1.83"
+    val NEOFORGE_VERSION: String = "21.1.90"
     val FABRIC_LOADER_VERSION: String = "0.16.9"
     val FABRIC_API_VERSION: String = "0.110.0+1.21.1"
 
@@ -10,7 +10,7 @@ object BuildConfig {
     val PARCHMENT_VERSION: String? = "2024.11.17"
 
     // https://semver.org/
-    var MOD_VERSION: String = "0.6.3"
+    var MOD_VERSION: String = "0.6.4"
 
     fun createVersionString(project: Project): String {
         val builder = StringBuilder()
