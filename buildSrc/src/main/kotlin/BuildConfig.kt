6c7585940f71027ed59078e5b6e1246b3d5dd8cb
JellySquid <jellysquid@pm.me>
2024-12-17 14:04:34 -0600

    Bump version and dependencies

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -2,15 +2,15 @@
 
 object BuildConfig {
     val MINECRAFT_VERSION: String = "1.21.4"
-    val NEOFORGE_VERSION: String = "21.4.3-beta"
+    val NEOFORGE_VERSION: String = "21.4.30-beta"
     val FABRIC_LOADER_VERSION: String = "0.16.9"
-    val FABRIC_API_VERSION: String = "0.110.5+1.21.4"
+    val FABRIC_API_VERSION: String = "0.112.1+1.21.4"
 
     // This value can be set to null to disable Parchment.
     val PARCHMENT_VERSION: String? = "2024.12.07"
 
     // https://semver.org/
-    var MOD_VERSION: String = "0.6.3"
+    var MOD_VERSION: String = "0.6.4"
 
     fun createVersionString(project: Project): String {
         val builder = StringBuilder()
