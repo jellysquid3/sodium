4a36e2996ad7ab9e18bbd31b8fd92e1f56a4a2ee
lowercasebtw <jejojora2@gmail.com>
2025-01-04 16:06:08 -0500

    Use right Nullable type

--- common/src/main/java/net/caffeinemc/mods/sodium/client/render/vertex/VertexConsumerUtils.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/client/render/vertex/VertexConsumerUtils.java
@@ -2,7 +2,8 @@ package net.caffeinemc.mods.sodium.client.render.vertex;
 
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.caffeinemc.mods.sodium.api.vertex.buffer.VertexBufferWriter;
-import org.jetbrains.annotations.Nullable;
+
+import javax.annotation.Nullable;
 
 public class VertexConsumerUtils {
     /**
